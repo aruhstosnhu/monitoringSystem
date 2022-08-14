3abea248dd38ef3dd6158c7d52e3cb8dc0163902
aruhstosnhu <92700885+aruhstosnhu@users.noreply.github.com>
2022-08-14 11:39:35 -0400

    Delete bin directory

--- bin/resources/generatehabitatreport.ps1
+++
@@ -1,5 +0,0 @@
-$habitat_data = get-childitem -file C:\Users\atruh\eclipse-workspace\monitoringSystem\src\resources\habitatreport.txt
-
-foreach ($file in $habitat_data) {
-    import-csv $file | export-csv "$($file.basename).csv" -NoTypeInformation 
-}
